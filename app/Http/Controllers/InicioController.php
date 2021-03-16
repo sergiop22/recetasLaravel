43bc9d2b948dc96f58767886fd2626e2f48c0788
sergiop22 <sergiopadillan97@gmail.com>
2021-03-16 10:41:40 -0600

    Agregue categorias a inicio

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Receta;
+use App\CategoriaReceta;
 
 class InicioController extends Controller
 {
@@ -13,6 +15,15 @@ class InicioController extends Controller
     	//obtener recetas nuevas
     	$nuevas = Receta::latest()->take(5)->get();
 
-    	return view('inicio.index', compact('nuevas'));
+    	//obtener todas las categorias
+    	$categorias = CategoriaReceta::all();
+
+    	//agrupas recetas por categoria
+    	$recetas = [];
+    	foreach ($categorias as $categoria) {
+    		$recetas[ Str::slug($categoria->nombre) ][] = Receta::where('categoria_id', $categoria->id)->take(3)->get();
+    	}
+
+    	return view('inicio.index', compact('nuevas', 'recetas'));
     }
 }
